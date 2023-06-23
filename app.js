d6b34db73f150c63a8a3ce5e02f0b7368094bd41
tushar_verma <vermatushar0101@gmail.com>
2023-06-23 15:27:25 +0530

    Issue #391 Update app.js / For smooth  scrolling of mouse from bottom to top. #319
    n the code above, the smoothScroll() function implements the smooth scrolling effect. It takes a target parameter specifying the target element selector (e.g., #target) and a duration parameter to control the scrolling speed.
    
    The window.addEventListener("wheel") section captures the mouse wheel event and prevents the default scrolling behavior. It then triggers the smoothScroll() function with the specified target element and duration when scrolling down.
    
    To use this code, make sure to replace #target in the window.addEventListener section with the selector of the element you want to scroll to. Adjust the duration value as needed to control the scrolling speed.
    
    Remember to place this code in the appropriate location within your app.js file, typically after setting up the Express server and before starting it with app.listen().

--- app.js
+++ app.js
@@ -1,15 +1,51 @@
-const express=require("express");
-const app=express();
-const bodyParser=require("body-parser");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-app.get("/",function(req,res){
-  res.sendFile(__dirname+"/index.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000,function(){
+// Smooth scrolling
+function smoothScroll(target, duration) {
+  const targetElement = document.querySelector(target);
+  const targetPosition = targetElement.offsetTop;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  let startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    const timeElapsed = currentTime - startTime;
+    const scrollAmount = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, scrollAmount);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  // Easing function for smooth scrolling
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
+
+// Smooth scroll to a specific element on mouse wheel
+window.addEventListener("wheel", function (event) {
+  event.preventDefault();
+  const delta = Math.sign(event.deltaY);
+  const targetElement = "#target"; // Replace with your target element selector
+  const duration = 1000; // Adjust the duration as needed (in milliseconds)
+  if (delta > 0) smoothScroll(targetElement, duration);
+});
+
+app.listen(3000, function () {
   console.log("Server has started on port 3000");
 });
